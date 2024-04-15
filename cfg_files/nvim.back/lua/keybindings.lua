59ef49c41964f8ba8c071b2520067208d5961ca4
Gonzalesderkaefer <Gonzalesderkaefer>
2024-04-15 18:17:46 +0200

    added movement between splits in nvim

--- cfg_files/nvim.back/lua/keybindings.lua
+++ cfg_files/nvim.back/lua/keybindings.lua
@@ -6,3 +6,8 @@ vim.keymap.set('n', '<C-i>', ':BufferNext<CR>')
 vim.keymap.set('n', '<C-W>', ':w | BufferClose<CR>')
 vim.keymap.set('n', '<leader>f', ':E<CR>')
 vim.keymap.set('n','<F12>', ':20 split | terminal<CR>')
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
