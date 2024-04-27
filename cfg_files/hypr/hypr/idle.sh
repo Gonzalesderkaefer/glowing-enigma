b460390642a3d3c031ef1a234bbdd4c9149001f6
Gonzalesderkaefer <Gonzalesderkaefer>
2024-04-27 21:36:35 +0200

    reroganized

--- cfg_files/hypr/hypr/idle.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-swayidle -w \
-  timeout 60 'hyprctl dispatch dpms off eDP-1' resume 'hyprctl dispatch dpms on eDP-1' \
-  timeout 300 'swaylock -f -c 000000' \
-  before-sleep 'swaylock -i Pictures/cube.png' &
--- cfg_files/waybar/modules/drivespace.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-df -H | awk '{print $3}' | sed -n 4p
