c13c934f745860f231b7a4bcacd41ef8dc645264
Gonzalesderkaefer <Gonzalesderkaefer>
2024-05-09 13:23:51 +0200

    added trash.sh fixes

--- cfg_files/nnn/plugins/trash.sh
+++ cfg_files/nnn/plugins/trash.sh
@@ -5,7 +5,7 @@ selection="$HOME/.config/nnn/.selection"
 printf "\n" >> $selection
 # interate through .selection file and move  to ~/.trash
 tr '\0' '\n' < $selection | while read -r file ; do
-    mv "$file" $HOME/.trash/
+    mv "$(echo $file | awk -F "/" '{print $NF}')" $HOME/.trash/
 done
 
 rm $selection
