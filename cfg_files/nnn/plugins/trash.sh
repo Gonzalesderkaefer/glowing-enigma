645f06624a48ea207ea5354a7f28276d8d67d652
Gonzalesderkaefer <Gonzalesderkaefer>
2024-05-12 14:10:16 +0200

    rewrote trash.sh

--- cfg_files/nnn/plugins/trash.sh
+++ cfg_files/nnn/plugins/trash.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-selection="$HOME/.config/nnn/.selection"
 
-printf "\n" >> $selection
-# interate through .selection file and move  to ~/.trash
-tr '\0' '\n' < $selection | while read -r file ; do
-    mv "$(echo $file | awk -F "/" '{print $NF}')" $HOME/.trash/
+# loading .selection into a varaible
+selection="$(cat $HOME/.config/nnn/.selection | tr '\0' '\n')"
+
+# iterate through $selection and move the files to .trash
+for i in $selection
+do
+    mv -f $i $HOME/.trash/
 done
-
-rm $selection
-
-
-
