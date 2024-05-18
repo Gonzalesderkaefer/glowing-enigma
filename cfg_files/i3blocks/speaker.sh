2581edc343e12a7e6f60499d8d2b80e54e9bd853
Gonzalesderkaefer <Gonzalesderkaefer>
2024-05-18 22:30:59 +0200

    added mute output

--- cfg_files/i3blocks/speaker.sh
+++ cfg_files/i3blocks/speaker.sh
@@ -4,7 +4,9 @@ SPEAKER_VOLUME="$(pactl get-sink-volume @DEFAULT_SINK@ | grep -E -o -m 1 "[0-9][
 
 get_output()
 {
+    ismuted="$(pactl get-sink-mute @DEFAULT_SINK@)"
     volume="$(pactl get-sink-volume @DEFAULT_SINK@ | grep -E -o -m 1 "[0-9][0-9][0-9]%|[0-9][0-9]%|[0-9]%" | head -1 | tr -d "%" )"
+    ([ $ismuted = "Mute: yes" ] && echo "󰝟 ") ||
     ([ $volume -ge 100 ]  && echo  "$volume  " )||
     ([ $volume -le 100 ] && [ $volume -ge 65 ] && echo  "$volume  " )||
     ([ $volume -le 65 ] && [ $volume -ge 35 ] && echo  "$volume  " )||
@@ -14,7 +16,7 @@ get_output()
 case $BLOCK_BUTTON in
     1)
         pactl set-sink-mute @DEFAULT_SINK@ toggle
-        echo "󰝟 "
+        
 
     4) 
         pactl set-sink-volume @DEFAULT_SINK@ +5%
