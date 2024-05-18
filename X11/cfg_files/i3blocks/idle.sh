52e1a98403af42cae2a7ef61049c643503cc52dd
Gonzalesderkaefer <Gonzalesderkaefer>
2024-05-18 16:25:21 +0200

    added idleinihibtor

--- X11/cfg_files/i3blocks/idle.sh
+++ X11/cfg_files/i3blocks/idle.sh
@@ -5,6 +5,6 @@ swayidle="$(pgrep swayidle)"
 
 case $BLOCK_BUTTON in
     1)
-        [ -z $swayidle ] && swayidle -w timeout 300 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' timeout 600 'swaylock -f -c 000000' before-sleep 'swaylock -f -c 000000' & || pkill swayidle
+        [ -z $swayidle ] && (swayidle -w timeout 300 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' timeout 600 'swaylock -f -c 000000' before-sleep 'swaylock -f -c 000000' & echo "󰅶 ") || (pkill swayidle & echo "󰾪 " )
         ;;
 esac
