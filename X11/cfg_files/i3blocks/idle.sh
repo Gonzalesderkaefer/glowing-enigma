beb92cc75cdd194b4ffc6fbaa423b83eb88f7235
Gonzalesderkaefer <Gonzalesderkaefer>
2024-05-18 19:14:53 +0200

    changed apporach for idle.sh

--- X11/cfg_files/i3blocks/idle.sh
+++ X11/cfg_files/i3blocks/idle.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
-swayidle="$(pgrep swayidle)"
+PIDFILE="/tmp/swayidle"
+pgrep swayidle > "$PIDFILE"
+
+start_idle()
+{
+    swayidle -w \
+    timeout 300 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
+    timeout 600 'swaylock -f -c 000000' \
+    before-sleep 'swaylock -f -c 000000' &
+    echo $! > "$PIDFILE"
+}
+
+stop_idle()
+{
+    if [ -f "$PIDFILE" ]; then
+        PID="$(cat "$PIDFILE" )"
+        kill $PID
+        rm "$PIDFILE"
+    fi
+}
 
 
 case $BLOCK_BUTTON in
     1)
-        [ -z $swayidle ] && (swayidle -w timeout 300 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' timeout 600 'swaylock -f -c 000000' before-sleep 'swaylock -f -c 000000' & echo "󰅶 ") || (pkill swayidle & echo "󰾪 " )
-        ;;
+        [ -f "$PIDFILE" ] && (stop_idle; echo "󰾪 ") || (start_idle; echo "󰅶 ")
+    *)
+        [ -f "$PIDFILE" ] && (echo "󰅶 ") || (echo "󰾪 ")
 esac
