9084b3af2db39777193acff3810ff50a85fdfdec
Gonzalesderkaefer <Gonzalesderkaefer>
2024-05-19 13:55:29 +0200

    speaker module should change colors now

--- cfg_files/i3blocks/speaker.sh
+++ cfg_files/i3blocks/speaker.sh
@@ -17,25 +17,21 @@ case $BLOCK_BUTTON in
     1)
         pactl set-sink-mute @DEFAULT_SINK@ toggle
         get_output
-        echo 
-        echo \#FF6666
+        [ "$(pactl get-sink-mute @DEFAULT_SINK@)" = "Mute: yes" ] && (echo; echo \#FF6666) || (echo; echo \#987ae6)
         ;;
     4) 
         pactl set-sink-volume @DEFAULT_SINK@ +5%
         get_output
-        echo
-        echo \#987ae6
+        [ "$(pactl get-sink-mute @DEFAULT_SINK@)" = "Mute: yes" ] && (echo; echo \#FF6666) || (echo; echo \#987ae6)
         ;;
     5)
         pactl set-sink-volume @DEFAULT_SINK@ -5%
         get_output
-        echo
-        echo \#987ae6
+        [ "$(pactl get-sink-mute @DEFAULT_SINK@)" = "Mute: yes" ] && (echo; echo \#FF6666) || (echo; echo \#987ae6)
         ;;
     *)
         get_output
-        echo
-        echo \#987ae6
+        [ "$(pactl get-sink-mute @DEFAULT_SINK@)" = "Mute: yes" ] && (echo; echo \#FF6666) || (echo; echo \#987ae6)
         ;;
 esac
 # "", "", ""
