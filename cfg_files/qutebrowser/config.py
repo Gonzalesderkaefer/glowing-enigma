e92776cae15c95b70c431651da1f1674a5a9de39
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2024-06-25 20:41:59 +0200

    added qutebrowser config

---
+++ cfg_files/qutebrowser/config.py
@@ -0,0 +1,9 @@
+
+config.load_autoconfig()
+
+c.url.searchengines = {
+    'DEFAULT':  'https://google.com/search?hl=en&q={}'
+}
+
+c.url.default_page = 'https://google.com' 
+c.url.start_pages = 'https://google.com'
